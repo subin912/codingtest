0cdbbe4054c3abb13ac1c8ae23d9701101ae486b
subin912 <100674285+subin912@users.noreply.github.com>
2026-01-13 10:23:20 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -8,3 +8,11 @@ def solution(arr):
         else:
             pass
     return result
+
+## 좀 더 깔끔풀이
+def solution(arr):
+    result = [arr[0]] #리스트로 시작
+    for i in range(1,len(arr)):
+        if arr[i] != arr[i-1]:
+            result.append(arr[i])
+    return result
