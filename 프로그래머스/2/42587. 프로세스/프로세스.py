718dcf327b86032197312be6ccc8418734b9931a
subin912 <100674285+subin912@users.noreply.github.com>
2026-01-19 23:50:29 +0900

    Update 프로세스.py

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -4,9 +4,9 @@ def solution(priorities, location):
     queue = deque() # 이제 여기에 (인덱스,우선순위)으로 들어가야함, 인덱스는 첨위치 기억용.
     for i in range(len(priorities)):
         queue.append((i, priorities[i]))
-				# 예: priorities = [2,1,3,2]
+		# 예: priorities = [2,1,3,2]
         # queue = [(0,2), (1,1), (2,3), (3,2)]
-        
+    ## 일단 위 과정은 인덱스, 우선순위 튜플 만들어주기 완료.    
     answer = 0
 		# 이제 시작
     while True: # 내가 찾는 프로세스 실행되면 return으로 종료
@@ -18,4 +18,18 @@ def solution(priorities, location):
             answer = answer + 1 
 
             if idx == location: #우리가 찾던 첨 위치 프로세스라면 그만.
+                return answer
+
+
+## 다른 풀이?
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
                 return answer
