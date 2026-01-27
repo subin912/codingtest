404c0fe0070bcf912f4fa140ae4cc0ddc25dfc0a
subin912 <100674285+subin912@users.noreply.github.com>
2026-01-27 17:29:28 +0900

    Update 구명보트.py

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,22 +2,29 @@ from collections import deque
 
 def solution(people, limit):
     answer = 0
-    people.sort() #정렬하기
-    people = deque(people)
-    #print (people) 
     
-    while len(people) > 0:
+    # 1. 몸무게 오름차순 정렬
+    people.sort()
+    
+    # 2. 리스트를 deque로 변환
+    people = deque(people)
+    
+    # 3. 사람이 남아 있는 동안 반복
+    while people:
+        # case 1: 한 명만 남았을 때
         if len(people) == 1:
-            people.pop()
-            answer = answer + 1 
+            people.pop()       # 마지막 한 명 태우고
+            answer += 1        # 보트 1개 사용
         else:
-            if people[0] + people[-1] > limit:
-                people.pop()
-                answer = answer + 1
+            # 가장 가벼운 사람 + 가장 무거운 사람
+            if people[0] + people[-1] <= limit:
+                # 같이 탈 수 있으면 둘 다 태우기
+                people.popleft()   # 가장 가벼운 사람 보트 탑승
+                people.pop()       # 가장 무거운 사람 보트 탑승
+                answer += 1
             else:
-                people.popleft()
-                #people.pop(0)
-                people.pop()
-                answer = answer + 1
-
+                # 둘이 같이 못 타면, 무거운 사람 혼자 보트 타야 함
+                people.pop()       # 가장 무거운 사람만 태움
+                answer += 1
+    
     return answer
