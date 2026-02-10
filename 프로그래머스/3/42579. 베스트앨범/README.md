1b417c90ebbeb43ff7989c051f4235243374936b
subin912 <100674285+subin912@users.noreply.github.com>
2026-02-10 17:13:00 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.13 MB, 시간: 0.07 ms
+메모리: 9.26 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 10일 03:12:18
+2026년 02월 10일 17:12:58
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,27 +4,24 @@ def solution(genres, plays):
     total = {} #장르별 총 재생수 저장
     
     for i in range(len(genres)): # i=노래고유번호
-        g = genres[i] #장르 ["classic", "pop", "classic"]
-        p = plays[i] #노래 재생 반복수  [500, 600, 150]
+        g = genres[i]
+        p = plays[i]
 
         # 이제 dict로 저장하기 위한 단계
         if g not in genre_dict: #처음보는 애면 넣어주자
             genre_dict[g] = []
             total[g] = 0   #처음은 0
 
-        genre_dict[g].append((p, i)) 
-        #{'classic': [(500, 0), (150, 2)], 'pop': [(600, 1)]}
-        total[g] = total[g] + p    #이 장르 총 재생수 더하기
-        #total["classic"] = total["classic"] + 500 => total = {"classic": 500}
+        genre_dict[g].append((p, i))
+        total[g] = total[g] + p    #장르 총 재생수 더하기
 
     #장르를 총재생수 기준으로 내림차순 정렬하기     
-    sorted_genres = sorted(total.items(), key=lambda x: x[1], reverse=True) #x[1] = 총재생수 650, 600, 120 등등
-    #[("classic", 650), ("pop", 600), ("jazz", 120)] 이런식으로 나옴. items() 하면 키,값 둘다 나온다!
+    sorted_genres = sorted(total.items(), key=lambda x: x[1], reverse=True) 
     
-    for g, c in sorted_genres: #g= 장르 name ,c = 그 장르의 총 재생수
-        songs = genre_dict[g] #장르의 노래들 꺼내기 #songs = [ (500,0), (150,2), (800,3) ]
+    for g, c in sorted_genres: #g= 장르name ,c = 그 장르의 총 재생수
+        songs = genre_dict[g] #장르의 노래들 꺼내기
         
-        # 재생수 내림차순, 재생수 같으면 고유번호 오름차순 (재생수 큰 노래부터, 재생수 같으면 번호 작은노래부터)
+        # 재생수 내림차순, 재생수 같으면 고유번호 오름차순
         songs.sort(key=lambda x: (-x[0], x[1])) 
 
         # 앞에서 최대 2개만 추가
@@ -34,4 +31,4 @@ def solution(genres, plays):
 
     return answer
 
-#즉, 인기장르별 차례로 가서, 그 장르에 인기 노래 2개씩만 뽑음.
+#즉, 인기장르붵 차례로 가서, 그 장르에 인기 노래 2개씩만 뽑음.
