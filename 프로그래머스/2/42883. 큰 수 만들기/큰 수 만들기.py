d40305d73a998951676b85e669840b787231ba5e
subin912 <100674285+subin912@users.noreply.github.com>
2026-02-10 15:54:10 +0900

    Update 큰 수 만들기.py

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -6,8 +6,9 @@ def solution(number, k):
     
     for i in number:
         #뒤에서 더 큰애 있으면 앞 숫자 제거
-        while stack and (k > 0) and stack[-1] < i: #조건 충족할때까지 못나가 
-            #stack은 비어있지않으면 true가 됨!!
+        while stack and (k > 0) and stack[-1] < i: #조건 충족할 때까지 못나가 
+            #stack은 비어있지않으면 true가 됨!! + k =0 되면 쫓아낼수없어서 그냥 append.
+            #stack[-1] < i : 내 앞에 써진 숫자가 나보다 작은가 -> 작으면 pop 실행.
             #새로 들어온 숫자가 stack 가장 최근숫자보다 클때 기존꺼들 제거 - 후입선출
             #ex)1924,2,94
             stack.pop()
@@ -21,3 +22,7 @@ def solution(number, k):
         stack = stack[:-k]
         
     return ''.join(stack)
+
+
+#내 뒤에 나보다 키(숫자)가 큰 친구가 오면, 나는 줄에서 빠져야 해!" (단, 기회는 딱 k번뿐이야!)
+#뒤에 나보다 큰 숫자가 기다리고 있다면, 내가 비켜줘야 전체 숫자가 커진다
