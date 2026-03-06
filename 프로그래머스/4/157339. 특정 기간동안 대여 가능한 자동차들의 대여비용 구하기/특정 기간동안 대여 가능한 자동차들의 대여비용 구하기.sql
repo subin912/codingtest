317938b3a936a78580c2b5a014ed570a91367a85
subin912 <100674285+subin912@users.noreply.github.com>
2026-03-06 16:49:04 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -4,12 +4,16 @@ FROM CAR_RENTAL_COMPANY_CAR R JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P ON R.CAR_T
 WHERE R.CAR_TYPE IN ('세단', 'SUV') AND P.DURATION_TYPE = '30일 이상' 
         AND R.CAR_ID NOT IN (SELECT CAR_ID 
                             FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                            WHERE START_DATE <='2022-11-30' AND END_DATE>='2022-11-01') ## 11월에 대여한것들 빼는 거 = NOT IN
-HAVING FEE BETWEEN 500000 AND 1999999
+                            WHERE START_DATE <='2022-11-30' AND END_DATE>='2022-11-01') ## 11월에 대여한 것들 빼는 거 = NOT IN
+HAVING FEE BETWEEN 500000 AND 1999999 #WHERE에서는 보통 SELECT에서 만든 별칭(FEE)을 직접 못 쓰기에 having에서 쓴다
 ORDER BY FEE DESC, R.CAR_TYPE ASC, R.CAR_ID DESC;
 
+# 대여 시작일이 11/30보다 같거나 이전 OR 대여종료일이 11/1보다 같거나 이후면
+# 대여 기간이 11월 구간(11/01~11/30)과 한 번이라도 겹친다는 뜻
+## R.CAR_ID NOT IN (겹치는 대여 기록이 있는 CAR_ID들) 
+## 즉,11월에 빌려진 적이 있는 차는 제외하고 11월에 빌릴 수 있는 차만 남긴다는 뜻
 
-
+####################
 # SELECT c.car_id, c.car_type, FLOOR(c.daily_fee*30*(1-p.discount_rate/100)) AS FEE
 # FROM CAR_RENTAL_COMPANY_CAR c 
 #     LEFT JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY h 
