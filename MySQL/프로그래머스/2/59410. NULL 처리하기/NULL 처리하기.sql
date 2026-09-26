9a3065bdec05fa97a2735a084ec3f3465135caa7
subin912 <100674285+subin912@users.noreply.github.com>
2026-09-26 23:03:44 +0900

    [level 2] Title: NULL 처리하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ MySQL/프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_TYPE,coalesce(name,"No name") AS NAME, SEX_UPON_INTAKE
+SELECT ANIMAL_TYPE,IFNULL(name,"No name") AS NAME, SEX_UPON_INTAKE
 FROM animal_ins
 ORDER BY animal_id;
